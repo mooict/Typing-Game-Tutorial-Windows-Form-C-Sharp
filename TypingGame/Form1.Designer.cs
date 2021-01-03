175424f94edf11b9f2d9d9784fee399001a67677
mooict <67914277+mooict@users.noreply.github.com>
2021-01-03 12:27:04 +0000

    Add files via upload

---
+++ TypingGame/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TypingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblright = new System.Windows.Forms.Label();
+            this.lblwrong = new System.Windows.Forms.Label();
+            this.lblword = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblright
+            // 
+            this.lblright.AutoSize = true;
+            this.lblright.Location = new System.Drawing.Point(13, 13);
+            this.lblright.Name = "lblright";
+            this.lblright.Size = new System.Drawing.Size(53, 13);
+            this.lblright.TabIndex = 0;
+            this.lblright.Text = "Correct: 0";
+            // 
+            // lblwrong
+            // 
+            this.lblwrong.AutoSize = true;
+            this.lblwrong.Location = new System.Drawing.Point(404, 13);
+            this.lblwrong.Name = "lblwrong";
+            this.lblwrong.Size = new System.Drawing.Size(61, 13);
+            this.lblwrong.TabIndex = 1;
+            this.lblwrong.Text = "Incorrect: 0";
+            // 
+            // lblword
+            // 
+            this.lblword.AutoSize = true;
+            this.lblword.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblword.Location = new System.Drawing.Point(171, 116);
+            this.lblword.Name = "lblword";
+            this.lblword.Size = new System.Drawing.Size(111, 42);
+            this.lblword.TabIndex = 2;
+            this.lblword.Text = "Word";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Press Enter to check the text with word";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(60, 215);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 49);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.checkGame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 408);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblword);
+            this.Controls.Add(this.lblwrong);
+            this.Controls.Add(this.lblright);
+            this.Name = "Form1";
+            this.Text = "Typing Game - Moo ICT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblright;
+        private System.Windows.Forms.Label lblwrong;
+        private System.Windows.Forms.Label lblword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TypingGame/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingGame
+{
+    public partial class Form1 : Form
+    {
+
+        string[] words = {"internet" , "programming", "mooict", "coding", "sky", "banana", "chair", "marbles", "puzzles"};
+
+        Random rnd = new Random();
+
+        int correct = 0;
+        int incorrect = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            lblword.Text = words[rnd.Next(0, words.Length) ];
+        }
+
+        private void checkGame(object sender, KeyEventArgs e)
+        {
+
+            if(e.KeyCode == Keys.Enter)
+            {
+
+                if(textBox1.Text == lblword.Text)
+                {
+                    correct++;
+                    lblword.Text = words[rnd.Next(0, words.Length)];
+                    textBox1.Text = null;
+                }
+                else
+                {
+                    incorrect++;
+                    lblword.Text = words[rnd.Next(0, words.Length)];
+                    textBox1.Text = null;
+                }
+
+                lblright.Text = "Correct: " + correct;
+                lblwrong.Text = "Incorrect: " + incorrect;
+
+            }
+
+
+        }
+    }
+}
